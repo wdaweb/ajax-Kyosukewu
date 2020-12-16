b89916dafc2d4a98b1b8a09bf154a8451eed2918
Kyosukewu <kyosukewu2020@gmail.com>
2020-12-16 16:17:36 +0800

    完成用ajax的CRUD

--- app2/api/query.php
+++ app2/api/query.php
@@ -9,6 +9,8 @@ foreach($rows as $row){
     echo "<td>{$row['classes']}</td>";
     echo "<td>{$row['num']}</td>";
     echo "<td>{$row['score']}</td>";
+    echo "<td><button type='button' data-id='{$row['id']}' onclick='del({$row['id']})'>刪除</button></td>";
+    echo "<td><button type='button' data-id='{$row['id']}' onclick='op({$row['id']})'>編輯</button></td>";
     echo "</tr>";
 }
 
